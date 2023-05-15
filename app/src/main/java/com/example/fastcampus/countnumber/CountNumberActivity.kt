57602f70078dbc36a29106dbef26c74b3f13f9a8
cjswntjd <cjswntjd@gmail.com>
2023-05-15 16:00:02 +0900

    [숫자세기] - 버튼 이벤트 추가

--- app/src/main/java/com/example/fastcampus/countnumber/CountNumberActivity.kt
+++ app/src/main/java/com/example/fastcampus/countnumber/CountNumberActivity.kt
@@ -2,11 +2,35 @@ package com.example.fastcampus.countnumber
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import com.example.fastcampus.R
+import com.example.fastcampus.databinding.ActivityCountNumberBinding
 
 class CountNumberActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityCountNumberBinding
+
+    private var number = 0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_count_number)
+        binding = ActivityCountNumberBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        with(binding) {
+
+            btnAdd.setOnClickListener {
+                Log.d(localClassName, "Button Add Clicked")
+
+                number++
+                tvCount.text = number.toString()
+            }
+
+            btnReset.setOnClickListener {
+                Log.d(localClassName, "Button Reset Clicked")
+
+                number = 0
+                tvCount.text = number.toString()
+            }
+        }
     }
 }
