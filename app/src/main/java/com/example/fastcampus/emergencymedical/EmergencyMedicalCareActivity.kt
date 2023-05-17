b362b7dee8670f12693b97e9a16b46d302d8c08f
cjswntjd <cjswntjd@gmail.com>
2023-05-17 10:55:41 +0900

    [응급 의료 정보] - 레이아웃 추가

--- app/src/main/java/com/example/fastcampus/emergencymedical/EmergencyMedicalCareActivity.kt
+++ app/src/main/java/com/example/fastcampus/emergencymedical/EmergencyMedicalCareActivity.kt
@@ -3,10 +3,16 @@ package com.example.fastcampus.emergencymedical
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.fastcampus.R
+import com.example.fastcampus.databinding.ActivityEmergencyMedicalCareBinding
 
 class EmergencyMedicalCareActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityEmergencyMedicalCareBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_emergency_medical_care)
+        binding = ActivityEmergencyMedicalCareBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
     }
 }
