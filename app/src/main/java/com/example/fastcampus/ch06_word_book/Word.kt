191a6bd1108c65d882fbef284a3f21238f93e728
cjswntjd <cjswntjd@gmail.com>
2023-05-22 10:33:43 +0900

    [단어장] - RecyclerView & 단어추가 레이아웃 구현

---
+++ app/src/main/java/com/example/fastcampus/ch06_word_book/Word.kt
@@ -0,0 +1,7 @@
+package com.example.fastcampus.ch06_word_book
+
+data class Word(
+    val text: String,
+    val mean: String,
+    val type: String
+)
--- app/src/main/java/com/example/fastcampus/ch06_word_book/WordBookActivity.kt
+++ app/src/main/java/com/example/fastcampus/ch06_word_book/WordBookActivity.kt
@@ -1,7 +1,11 @@
 package com.example.fastcampus.ch06_word_book
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.LinearLayout
+import android.widget.Toast
+import androidx.recyclerview.widget.DividerItemDecoration
 import com.example.fastcampus.R
 import com.example.fastcampus.databinding.ActivityWordBookBinding
 
@@ -13,5 +17,27 @@ class WordBookActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityWordBookBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        val dummyList = mutableListOf<Word>().apply {
+            add(Word("weather", "날씨", "명사"))
+            add(Word("honey", "꿀", "명사"))
+            add(Word("run", "실행하다", "동사"))
+        }
+
+        with(binding) {
+
+            recyclerView.apply {
+                addItemDecoration(DividerItemDecoration(applicationContext, LinearLayout.VERTICAL))
+                adapter = WordAdapter(dummyList).apply {
+                    click = {
+                        Toast.makeText(applicationContext, "Clicked $it", Toast.LENGTH_SHORT).show()
+                    }
+                }
+            }
+
+            addButton.setOnClickListener {
+                startActivity(Intent(applicationContext, AddActivity::class.java))
+            }
+        }
     }
 }
