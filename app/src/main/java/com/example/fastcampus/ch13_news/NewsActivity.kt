927f3fabdd6d8a11fd44ba16d848652a9f652f0f
cheonjoosung <cjswntjd@gmail.com>
2023-06-03 20:00:51 +0900

    [뉴스] - 카드 디자인 & 뉴스 데이터 바인딩

--- app/src/main/java/com/example/fastcampus/ch13_news/NewsActivity.kt
+++ app/src/main/java/com/example/fastcampus/ch13_news/NewsActivity.kt
@@ -1,17 +1,85 @@
 package com.example.fastcampus.ch13_news
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.example.fastcampus.R
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.fastcampus.ch13_news.network.NewsRss
+import com.example.fastcampus.ch13_news.network.NewsService
 import com.example.fastcampus.databinding.ActivityNewsBinding
+import com.tickaroo.tikxml.TikXml
+import com.tickaroo.tikxml.retrofit.TikXmlConverterFactory
+import org.jsoup.Jsoup
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.Retrofit
 
 class NewsActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityNewsBinding
 
+    private lateinit var newsAdapter: NewsAdapter
+
+    private val retrofit = Retrofit.Builder()
+        .baseUrl("https://news.google.com/")
+        .addConverterFactory(
+            TikXmlConverterFactory.create(
+                TikXml.Builder()
+                    .exceptionOnUnreadXml(false)
+                    .build()
+            )
+        )
+        .build()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityNewsBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        newsAdapter = NewsAdapter()
+
+        binding.newsRecyclerView.apply {
+            layoutManager = LinearLayoutManager(this@NewsActivity)
+            adapter = newsAdapter
+        }
+
+        val newsService = retrofit.create(NewsService::class.java)
+        newsService.mainFeed().enqueue(object : Callback<NewsRss> {
+            override fun onResponse(call: Call<NewsRss>, response: Response<NewsRss>) {
+                Log.e(localClassName, "onResponse ${response.body()?.channel?.items}")
+
+                val list = response.body()?.channel?.items.orEmpty().transform()
+                newsAdapter.submitList(list)
+
+                list.forEachIndexed { index, newsModel ->
+                    Thread {
+                        try {
+                            val jsoup = Jsoup.connect(newsModel.link)
+                                .get()
+
+                            val elements = jsoup.select("meta[property=og:image]")
+                            val ogImageNode = elements.find { node ->
+                                node.attr("property") == "og:image"
+                            }
+
+                            newsModel.imageUrl = ogImageNode?.attr("content")
+                        } catch (e: Exception) {
+                            Log.e(localClassName, "Error $e")
+                        }
+
+                        runOnUiThread {
+                            newsAdapter.notifyItemChanged(index)
+                        }
+                    }.start()
+                }
+
+            }
+
+            override fun onFailure(call: Call<NewsRss>, t: Throwable) {
+                Log.e(localClassName, "Failed ${t.message}")
+            }
+
+        })
     }
 }
---
+++ app/src/main/java/com/example/fastcampus/ch13_news/NewsModel.kt
@@ -0,0 +1,19 @@
+package com.example.fastcampus.ch13_news
+
+import com.example.fastcampus.ch13_news.network.NewsItem
+
+data class NewsModel(
+    val title: String,
+    val link: String,
+    var imageUrl: String? = null,
+)
+
+fun List<NewsItem>.transform(): List<NewsModel> {
+    return this.map {
+        NewsModel(
+            title = it.title ?: "",
+            link = it.link ?: "",
+            imageUrl = null
+        )
+    }
+}
