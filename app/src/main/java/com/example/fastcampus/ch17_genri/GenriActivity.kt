f9b15cdb93a396930f92bcc8f5ca7c8ba5fa903a
cjswntjd <cjswntjd@gmail.com>
2023-06-08 16:18:09 +0900

    [젠리] - 구글 지도 api 적용

--- app/src/main/java/com/example/fastcampus/ch17_genri/GenriActivity.kt
+++ app/src/main/java/com/example/fastcampus/ch17_genri/GenriActivity.kt
@@ -1,15 +1,24 @@
 package com.example.fastcampus.ch17_genri
 
-import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import com.example.fastcampus.R
 import com.example.fastcampus.databinding.ActivityGenriBinding
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
 import com.kakao.sdk.common.util.Utility
 
-class GenriActivity : AppCompatActivity() {
+
+class GenriActivity : AppCompatActivity(), OnMapReadyCallback {
 
     private lateinit var binding: ActivityGenriBinding
+
+    private lateinit var googleMap: GoogleMap
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,6 +28,21 @@ class GenriActivity : AppCompatActivity() {
         val keyHash = Utility.getKeyHash(this)
         Log.e(localClassName, keyHash)
 
-        startActivity(Intent(this, LoginActivity::class.java))
+        val mapFragment = supportFragmentManager
+            .findFragmentById(R.id.mapFragment) as SupportMapFragment
+        mapFragment.getMapAsync(this)
+    }
+
+    override fun onMapReady(map: GoogleMap) {
+        googleMap = map
+
+        // Add a marker in Sydney and move the camera
+        val sydney = LatLng(-34.0, 151.0)
+        googleMap.addMarker(
+            MarkerOptions()
+                .position(sydney)
+                .title("Marker in Sydney")
+        )
+        googleMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
     }
 }
