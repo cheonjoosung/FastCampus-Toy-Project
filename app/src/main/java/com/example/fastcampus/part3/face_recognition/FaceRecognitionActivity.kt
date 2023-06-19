01957d8b0634b675c6c518e13208b79b8ab76713
cjswntjd <cjswntjd@gmail.com>
2023-06-19 09:56:48 +0900

    [얼굴인식] - 메인 UI 추가

--- app/src/main/java/com/example/fastcampus/part3/face_recognition/FaceRecognitionActivity.kt
+++ app/src/main/java/com/example/fastcampus/part3/face_recognition/FaceRecognitionActivity.kt
@@ -2,6 +2,7 @@ package com.example.fastcampus.part3.face_recognition
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.view.isVisible
 import com.example.fastcampus.R
 import com.example.fastcampus.databinding.ActivityFaceRecognitionBinding
 
@@ -14,5 +15,11 @@ class FaceRecognitionActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
+
+        with(binding) {
+            startDetectButton.setOnClickListener {
+                it.isVisible = false
+            }
+        }
     }
 }
